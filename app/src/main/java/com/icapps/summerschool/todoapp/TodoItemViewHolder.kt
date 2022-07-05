88c5b9dc82006a1b7dca33c1cea736c5a149e20c
JefvdA <jef.v.d.a@live.be>
2022-07-05 15:19:10 +0200

    fix: moved strings to strings.xml
    Moved item index to strings.xml

--- app/src/main/java/com/icapps/summerschool/todoapp/TodoItemViewHolder.kt
+++ app/src/main/java/com/icapps/summerschool/todoapp/TodoItemViewHolder.kt
@@ -1,5 +1,6 @@
 package com.icapps.summerschool.todoapp
 
+import android.content.res.Resources
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -10,7 +11,7 @@ class TodoItemViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
     private val listNumberTextView: TextView = itemView.findViewById(R.id.list_item_todo_number)
 
     fun populateViewHolder(description: String, position: Int) {
-        listNumberTextView.text = position.toString()
+        listNumberTextView.text = itemView.resources.getString(R.string.list_item_todo_number, position + 1)
         listDescriptionTextView.text = description
     }
 }
