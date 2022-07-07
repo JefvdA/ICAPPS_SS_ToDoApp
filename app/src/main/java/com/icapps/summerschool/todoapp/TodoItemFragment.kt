2c7810cbf1989d494b15288b449659ed11ec7a93
JefvdA <jef.v.d.a@live.be>
2022-07-07 11:16:13 +0200

    feat: add addTaskDialog
    When in the fragment of an todoItem, you can create a new task with a dialog.
    Data is not saved

--- app/src/main/java/com/icapps/summerschool/todoapp/TodoItemFragment.kt
+++ app/src/main/java/com/icapps/summerschool/todoapp/TodoItemFragment.kt
@@ -1,9 +1,13 @@
 package com.icapps.summerschool.todoapp
+import android.content.DialogInterface
 import android.os.Bundle
+import android.text.InputType
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -49,9 +53,36 @@ class TodoItemFragment : Fragment() {
             it.adapter = taskListAdapter
             it
         }
-        addTaskFAB = binding.addTaskFAB
+        addTaskFAB = binding.addTaskFAB.let {
+            it.setOnClickListener {
+                showAddTaskDialog()
+            }
+            it
+        }
 
         (activity as AppCompatActivity).supportActionBar?.title = todoItem.description
+    }
+
+    private fun showAddTaskDialog() {
+        val addTaskEditText = EditText(binding.root.context).apply {
+            inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_FLAG_CAP_WORDS
+        }
+
+        AlertDialog.Builder(binding.root.context)
+            .setTitle(getString(R.string.create_todo_item_task_dialog_title, todoItem.description))
+            .setMessage(getString(R.string.create_todo_item_dialog_message))
+            .setView(addTaskEditText)
+            .setPositiveButton(getString(R.string.create)) { dialogInterface: DialogInterface, _: Int ->
+                val newTodoItemDescription = addTaskEditText.text.toString()
+                taskListAdapter.taskList.add(newTodoItemDescription)
+                taskListAdapter.notifyDataSetChanged()
+                dialogInterface.dismiss()
+            }
+            .setNegativeButton(getString(R.string.cancel)) { dialogInterface: DialogInterface, _: Int ->
+                dialogInterface.dismiss()
+            }
+            .create()
+            .show()
     }
 
     override fun onDestroyView() {
